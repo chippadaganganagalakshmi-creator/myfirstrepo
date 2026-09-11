bafd78ce9a913f51277420f79888a1db95808604
ganga <root@ip-172-31-46-122.eu-north-1.compute.internal>
2026-09-11 06:30:05 +0000

    second code added

--- pundu.java
+++ pundu.java
@@ -6,4 +6,53 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+----------------second code--------------
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        // Create a Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result;
+
+        // Perform the arithmetic calculation based on user choice
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                // Quick check to avoid dividing by zero
+                if (num2 == 0) {
+                    System.out.println("Error! Division by zero is not allowed.");
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid operator selected.");
+                return;
+        }
+
+        System.out.println("Result: " + num1 + " " + operator + " " + num2 + " = " + result);
+
+        // Close the scanner resource
+        scanner.close();
+    }
+}
 
