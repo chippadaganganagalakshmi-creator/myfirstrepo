757f54cf9ce42e7e8e1c9331d6d98a94c1cb1f84
ganga <root@ip-172-31-46-122.eu-north-1.compute.internal>
2026-09-11 06:24:32 +0000

    code added

---
+++ pundu.java
@@ -0,0 +1,9 @@
+// The file name must match the public class name exactly (HelloWorld.java)
+public class HelloWorld {
+    // The main method is the entry point of any Java application
+    public static void main(String[] args) {
+        // Prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
+
